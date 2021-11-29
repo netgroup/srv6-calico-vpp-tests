31ed46fae88da8ce793eedded2857450fbffb2b6
Francesco Lombardo <franclombardo@gmail.com>
2021-11-29 09:55:08 +0100

    create veth pair script

--- scripts/create-veth-pair.sh
+++ scripts/create-veth-pair.sh
@@ -5,10 +5,12 @@
 HOST=$1
 INDEX=$2
 NETNS_PID=$(ps ax | grep mininet:$HOST | grep bash | awk '{print $1}')
-V_SWITCH=$3
+VM_NAME=$3
 VETH_NS_IP6=$4
-VETH_NS_NAME="veth${INDEX}"
-
+VETH_NS_NAME="veth${HOST}${INDEX}"
+NET_NAME=$(sudo virsh domiflist $VM_NAME | grep srv6-calico-vpp-tests | awk '{print $3}')
+V_SWITCH=$(sudo virsh net-info $NET_NAME | grep Bridge | awk '{print $2}')
+echo "$NET_NAME $V_SWITCH"
 sudo nsenter -t "$NETNS_PID" -n -m -- ip link add $VETH_NS_NAME type veth peer name $VETH_NS_NAME netns 1
 sudo ip link set $VETH_NS_NAME up
 # connect the veth0 to L2 switch
@@ -17,5 +19,4 @@ sudo ip link set $VETH_NS_NAME master $V_SWITCH
 sudo nsenter -t "$NETNS_PID" -n -m -- ip link set $VETH_NS_NAME up
 # assign IP6 to the veth0 inside the NS
 sudo nsenter -t "$NETNS_PID" -n -m -- ip addr add $VETH_NS_IP6 dev $VETH_NS_NAME
-
 
